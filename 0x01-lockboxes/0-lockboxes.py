d9439697ea752a73bed3055e53df64da9ff57ce7
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-07 02:11:31 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,21 @@
-#!/usr/env python3
-"""
-A functio that unlocks boxes
-"""
+#!/usr/bin/python3
+""" a function that unlock boxes """
 
 
 def canUnlockAll(boxes):
     """
     A function that unlock boxes
     """
+
     if len(boxes) < 1:
         return False
-    # Create a list of keys
+    # Create a listofKeys
     keys = [0]
     # Set of unlocked boxes
     index = 0
-    # if we have key
+    # if we have a key
     while index < len(keys):
+        # Open the box with the key
         if len(boxes[keys[index]]) < 1:
             index += 1
             continue
@@ -24,6 +24,7 @@ def canUnlockAll(boxes):
                 if element < len(boxes):
                     keys.append(element)
         index += 1
+    # Check the keys
     if len(keys) == len(boxes):
         return True
     return False
