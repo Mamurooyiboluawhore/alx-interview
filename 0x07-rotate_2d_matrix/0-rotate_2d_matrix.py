51dac940d38e05fd70e4f0d1e88f397936a217de
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-08-17 23:36:31 +0000

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,6 +11,4 @@ def rotate_2d_matrix(matrix):
     for i in range(mat):
         for j in range(i, mat):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-#    for i in range(mat):
         matrix[i].reverse()
